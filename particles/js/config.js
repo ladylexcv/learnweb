715f9400c71ad2122202a00798ec34202a3506db
ladyLexcv <cpla_army@163.com>
2018-11-04 01:35:07 +0800

    particles-js练习

---
+++ particles/js/config.js
@@ -0,0 +1,82 @@
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#fff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "widht": 0,
+        "color": "#000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+    },
+    "opactiy": {
+      "value": 0.4,
+      "random": false,
+      // "anim": {
+      //   "enable": true,
+      //   "spend": 500,
+      //   "opactiy_min": 0.1,
+      //   "sync": false
+      // }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      // "anim": {
+      //   "enable": true,
+      //   "spend": 3,
+      //   "sync": false
+      // }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 10,
+      "color": "#fff",
+      "opacity": 0,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "spend": 10,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "move_bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 3000,
+        "rotateY": 1500,
+      }
+    },
+    // "events": {
+    //   "onhover": {
+    //     "enable": true,
+    //     "mode": "repulse",
+    //   },
+    //   "onclick": {
+    //     "enable": true,
+    //     "mode": "remove"
+    //
+    //   },
+    //   "modes": {
+    //     "repulse": {
+    //       "distance": 200,
+    //       "duration": 1.2
+    //     }
+    //   }
+    // },
+    "retina_detect": true
+  }
+})
